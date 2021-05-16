973e536ab27987961c52613bb8442be2bf5de6ad
Mira Chami <miramc149@gmail.com>
2021-05-16 20:12:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,14 @@ Rails.application.routes.draw do
   get '/birds' => 'birds#index'
   get '/birds/:id' => 'birds#show' # new
 end
+class BirdsController < ApplicationController
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+ 
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+end
